c2815d542150a0689c013f683bd05668da28d4c7
Mohammadabdulrab <mohammadabdulrab78760@gmail.com>
2025-01-12 15:27:50 +0530

    Add Float variable

--- src/dsabasics/BasicsOfDsa.java
+++ src/dsabasics/BasicsOfDsa.java
@@ -8,7 +8,18 @@ package dsabasics;
 
 public class BasicsOfDsa {
     public static void main(String[] args) {
-        int var=10;
-        System.out.println("My num is "+var);
+//        int var=10;
+//        System.out.println("My num is "+var);
+
+// Question 2
+
+//  Variable Practice Question 2
+//  Declare a variable of type float named var and initialize its value to 10.45
+//
+//  After that re-initialize its value to 20.55 and print the variable var
+
+        float var=10.45f;
+        var=20.55f;
+        System.out.println(var);
     }
 }
