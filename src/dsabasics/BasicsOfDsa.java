5c85b1d609ff3f02525bbce9c1fafb1a0a3e9ee7
Mohammadabdulrab <mohammadabdulrab78760@gmail.com>
2025-01-12 15:41:48 +0530

    Add Celcius to Farhanhiet conversion

--- src/dsabasics/BasicsOfDsa.java
+++ src/dsabasics/BasicsOfDsa.java
@@ -1,4 +1,5 @@
 package dsabasics;
+import java.util.*;
 
 //Question 1
 
@@ -18,8 +19,47 @@ public class BasicsOfDsa {
 //
 //  After that re-initialize its value to 20.55 and print the variable var
 
-        float var=10.45f;
-        var=20.55f;
-        System.out.println(var);
+//        float var=10.45f;
+//        var=20.55f;
+//        System.out.println(var);
+
+        //Question 3
+// Celsius to Fahrenheit 2
+//  Given a temperature in Celsius, output the fahrenheit equivalent of it.
+//
+//   Note
+//
+//   Print complete answer up to 6 decimal places.
+//
+//   To do so, you can use the System.out.printf() function in Java
+//
+//   For example,
+//
+//   float myFloat = 76.45678f;
+//   System.out.printf("%.3f", myFloat);
+//   %.3f is the format specifier used in System.out.printf. It indicates that the value of myFloat should be printed as a floating-point number with three decimal places. The output is rounded to three decimal places: 76.457.
+//
+//                Input Format
+//        Input consists of 1 line of a decimal number.
+//
+//                Output Format
+//        Output consists of a single line of temperature in Fahrenheit.
+//
+//        EXAMPLE 1
+//        Input:
+//
+//        32
+//        Output::
+//
+//        89.600000
+//        EXPLANATION:
+//
+//        To convert temperatures in degrees Celsius to Fahrenheit, multiply it by 9/5 or 1.8 and add 32.
+        //C=5/9(F-32)
+
+        Scanner sc= new Scanner(System.in);
+//        double cel=sc.nextDouble();
+//        double far=(cel*9)/5+32;
+//        System.out.printf("%.6f", far);
     }
 }
