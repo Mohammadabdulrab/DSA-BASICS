96147cfdb5b00da304226dbd7bd48af31f2ac402
Mohammadabdulrab <mohammadabdulrab78760@gmail.com>
2025-01-12 15:20:15 +0530

    Add Variable declaration

---
+++ src/dsabasics/BasicsOfDsa.java
@@ -0,0 +1,14 @@
+package dsabasics;
+
+//Question 1
+
+//Declare a variable named var and assign it a value of 10.
+// Then print the string “My num is ” (without quotes)
+// followed by the value stored in the variable.
+
+public class BasicsOfDsa {
+    public static void main(String[] args) {
+        int var=10;
+        System.out.println("My num is "+var);
+    }
+}
