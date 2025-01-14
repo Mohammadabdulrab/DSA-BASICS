81b8d720c34836267304f9b586319f19a9e9b557
Mohammadabdulrab <mohammadabdulrab78760@gmail.com>
2025-01-14 13:28:01 +0530

    Add leap yaer

--- src/fundamentalsOfJava/Conditionals.java
+++ src/fundamentalsOfJava/Conditionals.java
@@ -128,6 +128,26 @@ public class Conditionals {
 //            System.out.println("31");
 //        }
 
+//     ..................Question 5...............
+
+
+//        Leap Year
+//        Given a year, find if it is a leap year.
+//
+//                A Leap year is a year that is multiple of 4. However, multiples of 100 except for the multiples of 400 are not leap years.
+//
+//        Your task is to print 1 or 0 depending upon if the year is a leap year or not.
+
+//        int year=sc.nextInt();
+//        if(year%4==0 && year%100!=0){
+//            System.out.println("1");
+//        }
+//        else if(year%100==0 && year%400==0){
+//            System.out.println("1");
+//        }
+//        else {
+//            System.out.println("0");
+//        }
 
 
 
