71b239d4bdbf1725f5c69dc6ba2a16f000467a62
Mohammadabdulrab <mohammadabdulrab78760@gmail.com>
2025-01-15 21:11:46 +0530

    Add number is prime or not

---
+++ src/loops/CheckPrime.java
@@ -0,0 +1,33 @@
+package loops;
+
+import java.util.Scanner;
+public class CheckPrime {
+    public static void main(String[] args) {
+
+        // ................Question............
+
+//        Check Prime
+
+//        Write a program that inputs a positive integer N.
+//        It should then output a message indicating whether the number is a
+//        prime number or not.
+
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        int count=0;
+        int i=1;
+        while(i<=n){
+            if(n%i==0){
+                count=count+1;
+            }
+            i++;
+        }
+        if(count==2){
+            System.out.println(n+" is a prime number");
+        }
+        else{
+            System.out.println(n+" is not a prime number");
+        }
+
+    }
+}
