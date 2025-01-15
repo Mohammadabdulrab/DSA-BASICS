07d938f853088be1f5ed1afcad3209791fdfe903
Mohammadabdulrab <mohammadabdulrab78760@gmail.com>
2025-01-15 16:15:41 +0530

    Add Print 1 to 1o

---
+++ src/loops/Print1to10.java
@@ -0,0 +1,21 @@
+package loops;
+
+public class Print1to10 {
+    public static void main(String[] args) {
+
+
+        // .......... Question...........
+
+//    Print 1 to 10
+//    Write a program to print numbers from 1 to 10.
+//
+//    NOTE: No input is required, just print numbers from 1 to 10.
+
+
+        int i=1;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
