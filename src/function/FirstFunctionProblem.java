440087516c13fc97705965845cb8a1d3941263a1
Mohammadabdulrab <mohammadabdulrab78760@gmail.com>
2025-01-19 21:20:28 +0530

    Add create first function fun

---
+++ src/function/FirstFunctionProblem.java
@@ -0,0 +1,23 @@
+package function;
+
+
+
+public class FirstFunctionProblem {
+
+
+    //................Question....................
+
+//    Function Problem 1
+
+//    Write a function such that a function fun will be called and
+//    "I am another function" will be printed.
+
+    public static void fun(){
+        System.out.println("I am another function");
+    }
+
+
+    public static void main(String[] args) {
+        fun();
+    }
+}
